42a62fdf09c89f7f93c4177c8843fa063460b496
Oluseyi <oluseyiolarewaju@outlook.com>
2025-12-09 04:07:08 +0000

    Fix for python versions and repo name

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="stintagents-evals",
+    name="stintagents-eval",
     version="0.1.0",
     author="Oluseyi",
     description="Multi-agent voice interaction system for employee onboarding",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/drOluOla/stintagents-evals",  
+    url="https://github.com/drOluOla/stintagents-eval",  
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.8,<3.12",
     install_requires=[
         "openai>=1.59.7",
         "openai-agents[voice]==0.4.2",
